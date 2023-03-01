eb1dc2c677c9075b732c375614f49da693e662d6
KyleM73 <kyle.morgenstein@gmail.com>
2023-03-01 01:55:41 -0600

    restructure

---
+++ Imitation/__init__.py
@@ -0,0 +1,1 @@
+from DAgger.DAgger import DAgger
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Imitation',
+      version='0.1.0',
+      install_requires=[
+            'torch',
+            'matplotlib',
+            'pybullet'
+            ]
+)
